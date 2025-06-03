5707a558298b06c96bdb3a9c1fa41ff139fdf396
Saket <codingwithsaket@gmail.com>
2025-06-04 01:17:24 +0530

    Initial setup for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig = {
   reactStrictMode: true,
+  output: isGitHubPages ? 'export' : undefined,
+  basePath: isGitHubPages ? '/saketpsingh.github.io' : '',
+  assetPrefix: isGitHubPages ? '/saketpsingh.github.io/' : '',
   images: {
     domains: ["localhost"],
     remotePatterns: [
